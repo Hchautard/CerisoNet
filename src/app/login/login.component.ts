c0845d6d8410626ae326b639f442d0b677023196
Hugo <chautardhugo@gmail.com>
2025-02-08 12:43:31 +0100

    Create login link between form and class

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit() { }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log('Formulaire soumis avec succès', this.loginForm.value);
+    } else {
+      console.log('Formulaire invalide');
+    }
+  }
 }
