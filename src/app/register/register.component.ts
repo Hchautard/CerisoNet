889285bba0b27cc1ba42282977b2980ae8530c9c
Hugo <chautardhugo@gmail.com>
2025-02-08 12:38:20 +0100

    Create register link between form and class

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit() { }
+
+  register() {
+    if (this.registerForm.valid) {
+      console.log('Formulaire soumis avec succès', this.registerForm.value);
+    } else {
+      console.log('Formulaire invalide');
+    }
+  }
 }
