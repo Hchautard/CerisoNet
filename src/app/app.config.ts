fca25dea515f15844e0f1a46860ae0086d4c6973
Hugo <hugo.chautard@etu.univ-amu.fr>
2025-03-11 15:14:12 +0100

    Reduce technicity

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 
 /* Application configuration
-* provideClientHydration est un service qui permet de récupérer les données du serveur pour les injecter dans l'application côté client.
 * provideHttpClient est un service qui permet de configurer le client HTTP pour effectuer des requêtes HTTP.
 * provideRouter est un service qui permet de configurer le routage de l'application.
 */
-
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withViewTransitions()),
-    provideClientHydration(),
+    provideRouter(routes),
     provideHttpClient(withFetch(), withInterceptors([]))
   ]
 };
